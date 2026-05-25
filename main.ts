c3e4a63f4743cec97f680b88f149c8b2d40c23e0
quest-and-vcs-rnd <quest.and.vcs.rnd@gmail.com>
2026-05-24 19:24:44 -0700

    *** v0.7.0 *** Save in Blocks Mode

--- main.ts
+++ main.ts
@@ -2018,7 +2018,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    _system_Github_VER_STRING = "v0.6.3"
+    _system_Github_VER_STRING = "v0.7.0"
     quest_Note_1.quest_Show_String_For_Note_Small_Func(
     "©️ 2026 Quest Institute. All rights reserved."
     )
